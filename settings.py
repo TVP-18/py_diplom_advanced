c0cda845aa38960583bd7d432d1b81d0b3fcfd7f
Tatiana Popova <tatiana_mail@bk.ru>
2021-07-11 20:52:03 +0700

    update settings.py

--- settings.py
+++ settings.py
@@ -5,26 +5,5 @@ config = dotenv_values(".env")
 
 TOKEN_GROUP = config['TOKEN_GROUP']
 TOKEN_USER = config['TOKEN_USER']
-
-# def check_token():
-#     pass
-
-
-
-# def load_param():
-#     config = dotenv_values(".env")
-#
-#     if 'token_group' not in config:
-#         print('Для работы приложения необходим токен сообщества')
-#         return False
-#
-#     os.environ['token_group'] = config['token_group']
-#     print(os.environ['token_group'])
-#
-#     return True
-#
-#
-# def save_param(key, value):
-#     os.environ[key] = value
-#     set_key(".env", key, os.environ[key])
+DATABASE_URL = config['DATABASE_URL']
 
