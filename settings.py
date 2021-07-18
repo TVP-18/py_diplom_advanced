2d683ef06b0c84f558ae995b2ca33237216fe821
Tatiana Popova <tatiana_mail@bk.ru>
2021-07-18 09:48:37 +0700

    Добавлен файл '.env.example' с примером настроек. Загрузка настроек переделана.

--- settings.py
+++ settings.py
@@ -1,9 +1,37 @@
 import os
-from dotenv import dotenv_values, set_key
+from dotenv import load_dotenv, find_dotenv, set_key
 
-config = dotenv_values(".env")
+# имя файла настроек по умолчанию
+ENV = '.env'
 
-TOKEN_GROUP = config['TOKEN_GROUP']
-TOKEN_USER = config['TOKEN_USER']
-DATABASE_URL = config['DATABASE_URL']
 
+def create_key(text, key, file_name=ENV):
+    while True:
+        key_value = input(f'{text}: ')
+        if key_value == '':
+            print(f'Значение не может быть пустым!')
+        else:
+            break
+
+    os.environ[key] = key_value
+    set_key(file_name, key, os.environ[key])
+
+
+# читаем настройки из файла, если их нет, то запрашиваем у пользователя
+exists_env = find_dotenv(ENV)
+
+if exists_env != '':
+    load_dotenv(exists_env)
+
+else:
+    create_key('Введите ключ доступа сообщества', 'TOKEN_GROUP')
+    create_key('Введите ключ доступа пользователя', 'TOKEN_USER')
+    create_key('Введите строку подключения к БД', 'DATABASE_URL')
+
+TOKEN_GROUP = os.getenv('TOKEN_GROUP')
+TOKEN_USER = os.getenv('TOKEN_USER')
+DATABASE_URL = os.getenv('DATABASE_URL')
+
+
+if __name__ == "__main__":
+    print(TOKEN_GROUP, TOKEN_USER, DATABASE_URL)
