9e17cd3a0470ed19a32ddfef9ce9bf2891d9a7a1
Tatiana Popova <tatiana_mail@bk.ru>
2021-07-18 16:26:48 +0700

    Подключение к БД и функции для работы с БД

--- db/db_work.py
+++ db/db_work.py
@@ -5,9 +5,7 @@ from sqlalchemy.orm import sessionmaker, relationship
 from settings import DATABASE_URL
 
 Base = declarative_base()
-
 engine = sq.create_engine(DATABASE_URL)
-
 Session = sessionmaker(bind=engine)
 
 
@@ -17,12 +15,34 @@ class UsersVk(Base):
     id = sq.Column(sq.Integer, primary_key=True)
     settings = sq.Column(sq.String)
 
+    def __init__(self, id):
+        self.id = id
+
+    def __repr__(self):
+        return f'{self.id}'
+
+
+def exists_user(user_id):
+    print(user_id)
+    with Session() as session:
+        result = session.query(UsersVk).filter(UsersVk.id == user_id)
+
+    return False if result.first() is None else True
+
+
+def add_user(user):
+    with Session() as session:
+        session.add(user)
+        session.commit()
+
 
 if __name__ == '__main__':
-    # pass
-    session = Session()
-    cur_user = UsersVk(id=111, settings='dfsdfs')
+    # Base.metadata.create_all(engine)
 
-    session.add(cur_user)
-    session.commit()
+    # with Session() as session:
+    #     user = UsersVk(650124345)
+    #     session.add(user)
+    #     session.commit()
+
+    print(exists_user(6))
 
