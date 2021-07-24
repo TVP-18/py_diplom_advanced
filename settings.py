39ea55efe7ab165dea5da36e226c755747ae59de
Tatiana Popova <tatiana_mail@bk.ru>
2021-07-24 22:42:55 +0700

    Изменен ввод настроек для подключения БД. Теперь отдельно вводятся: адрес сервера, порт сервера, имя БД, имя пользователя, пароль.

--- settings.py
+++ settings.py
@@ -6,15 +6,31 @@ ENV = '.env'
 
 
 def create_key(text, key, file_name=ENV):
-    while True:
-        key_value = input(f'{text}: ')
-        if key_value == '':
-            print(f'Значение не может быть пустым!')
-        else:
-            break
+    key_value = input_value(f'{text}: ')
 
     os.environ[key] = key_value
     set_key(file_name, key, os.environ[key])
+
+
+def create_key_url(key, file_name=ENV):
+    print('Настройки для подключения БД')
+    hostname = input_value('Адрес сервера: ')
+    port = input_value('Порт сервера: ')
+    database_name = input_value('Имя БД: ')
+    user = input_value('Имя пользователя: ')
+    password = input_value('Пароль: ')
+
+    os.environ[key] = f'postgresql://{user}:{password}@{hostname}:{port}/{database_name}'
+    set_key(file_name, key, os.environ[key])
+
+
+def input_value(text):
+    while True:
+        value = input(text)
+        if value == '':
+            print(f'Значение не может быть пустым!')
+        else:
+            return value
 
 
 # читаем настройки из файла, если их нет, то запрашиваем у пользователя
@@ -26,7 +42,8 @@ if exists_env != '':
 else:
     create_key('Ключ доступа сообщества', 'TOKEN_GROUP')
     create_key('Ключ доступа пользователя', 'TOKEN_USER')
-    create_key('Строка подключения к БД', 'DATABASE_URL')
+    create_key_url('DATABASE_URL')
+
 
 TOKEN_GROUP = os.getenv('TOKEN_GROUP')
 TOKEN_USER = os.getenv('TOKEN_USER')
